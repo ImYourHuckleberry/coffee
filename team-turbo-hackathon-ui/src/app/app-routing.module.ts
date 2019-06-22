83e1e3082b9d3f97f7df55fa5e0594dd42627594
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-06-22 12:48:09 -0500

    ready for service like a lady of the night

--- team-turbo-hackathon-ui/src/app/app-routing.module.ts
+++ team-turbo-hackathon-ui/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HallOfShameComponent } from './hall-of-shame/hall-of-shame.component';
+import { HallOfFameComponent } from './hall-of-fame/hall-of-fame.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './shared/components/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: DashboardComponent },
+  { path: 'shame', component: HallOfShameComponent },
+  { path: 'fame', component: HallOfFameComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- team-turbo-hackathon-ui/src/app/shared/components/most-recent-row/most-recent-row.component.ts
+++ team-turbo-hackathon-ui/src/app/shared/components/most-recent-row/most-recent-row.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ViewDetailsComponent } from '../modal/view-details/view-details.component';
 
 @Component({
   selector: 'app-most-recent-row',
@@ -9,11 +11,30 @@ import { Component, OnInit } from '@angular/core';
 
 export class MostRecentRowComponent implements OnInit {
 
+  name: string;
+
   //test array of users
   public dudes = [{ firstname: 'joe', lastname: 'doe' }, { firstname: 'yoe', lastname: 'poe' }, { firstname: 'xoe', lastname: 'doe' }, { firstname: 'soe', lastname: 'doe' }, { firstname: 'koe', lastname: 'doe' }]
-  constructor() { }
+  constructor(public dialog: MatDialog) {
+
+  }
 
   ngOnInit() {
   }
+
+  viewDetails() {
+    const dialogRef = this.dialog.open(ViewDetailsComponent, {
+      width: '30%',
+      data: { name: this.name }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('dialog closed, i guess');
+      this.name = result;
+      console.log(this.name)
+    })
+  }
+
+
 
 }
